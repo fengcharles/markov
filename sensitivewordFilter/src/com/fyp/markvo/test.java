e0d38e20617982e4c456d944cdacd5b2a804b0d1
fyp <charles@mogoroom.com>
2016-08-27 11:07:47 +0800

    敏感词过滤

---
+++ sensitivewordFilter/src/com/fyp/markvo/test.java
@@ -0,0 +1,18 @@
+package com.fyp.markvo;
+
+import java.util.HashMap;
+import java.util.Map;
+ 
+public class test {
+ 
+	public static void main(String[] args) {
+		
+		
+		Map<String, Object> map = new HashMap<>();
+		
+		
+		
+		
+	}
+	
+}
